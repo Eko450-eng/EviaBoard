752a6f904be0cd26e79476c0304244bdbaf48d4a
Eko450-eng <ekrem@wipdesign.de>
2024-09-26 23:41:32 +0200

    Updated some querys

--- src/routes/knowledgeboard/+page.svelte
+++ src/routes/knowledgeboard/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import Plus from "svelte-radix/Plus.svelte";
+    import Cross from "svelte-radix/Cross1.svelte";
     import { onMount, tick } from "svelte";
     import * as Command from "../../lib/components/ui/command/index.js";
     import * as Popover from "../../lib/components/ui/popover/index.js";
@@ -14,6 +15,7 @@
     import { getDb } from "../../lib/db";
     import { redirect } from "@sveltejs/kit";
     import type Surreal from "surrealdb";
+    import { RecordId } from "surrealdb";
 
     const topics = [
         {
@@ -52,49 +54,69 @@
     }
 
     type post = {
+        id?: any;
         title: string;
         body: string;
-        topic: string;
         solution: string;
+        owner: { name: string; id: string };
+    };
+
+    type user = {
+        email: string;
+        id: any;
+        name: string;
+        password: string;
     };
 
     let posts: post[] = [];
     let DB: Surreal | undefined;
+    let token: string | null;
+    let user: user[] | undefined;
+    let user_id: string | undefined | null;
 
     onMount(async () => {
-        let token = localStorage.getItem("user_token");
+        token = localStorage.getItem("user_token");
         if (!token) {
             redirect(300, "/");
         } else {
             DB = await getDb();
-            let query = "select body, title, topic.name as topic from posts";
+            let query =
+                "select id, body, title, owner.id, owner.name from posts";
             let postsList: Array<Array<post>> = (await DB?.query(
                 query,
             )) as unknown as Array<Array<post>>;
             posts = postsList[0];
+            user = await DB?.query<user[]>("$auth");
+            const queryUuid = await DB?.live("posts", (action, result) => {
+                if (action === "CLOSE") return;
+            });
+            await DB?.subscribeLive(queryUuid!, async (action, result) => {
+                let postsList: Array<Array<post>> = (await DB?.query(
+                    query,
+                )) as unknown as Array<Array<post>>;
+                posts = postsList[0];
+            });
+            user_id = `user:${user![0].id}`;
         }
     });
 
     let postData: post = {
         title: "Title",
         body: "Markdown support soooon!",
-        topic: "",
         solution: "test",
+        owner: { id: "", name: "" },
     };
 
+    async function deletePost(id: string) {
+        await DB?.delete(new RecordId("posts", id));
+    }
+
     async function addPost() {
-        let topic = null;
-        if (selectedValue !== "Select a Topic") {
-            topic = selectedValue;
-        }
-
-        // TODO Change this code to use topics instead of null
-
         await DB?.query(` CREATE posts CONTENT{
             title:  "${postData.title}",
             body:  "${postData.body}",
             solution:  "${postData.solution}",
-            topic: topic
+            owner: ${user_id},
         }`);
         open = false;
     }
@@ -150,15 +172,24 @@
 <div class="p-4">
     <Popover.Root bind:open let:ids>
         <Popover.Trigger asChild let:builder>
-            <Button
-                builders={[builder]}
-                variant="outline"
-                role="combobox"
-                aria-expanded={open}
-                class="w-[200px] justify-between"
-            >
-                {selectedValue}
-            </Button>
+            <div class="flex p-4 gap-1 items-center">
+                <Button
+                    builders={[builder]}
+                    variant="outline"
+                    role="combobox"
+                    aria-expanded={open}
+                    class="w-[200px] justify-between"
+                >
+                    {selectedValue}
+                </Button>
+                <Button
+                    on:click={() => (selectedValue = "Select a Topic")}
+                    class="justify-between"
+                    variant="outline"
+                >
+                    <Cross />
+                </Button>
+            </div>
         </Popover.Trigger>
         <Popover.Content class="w-[200px] p-0">
             <Command.Root>
@@ -183,11 +214,11 @@
 
     <div class="grid grid-cols-[repeat(auto-fit,_minmax(350px,_1fr))] gap-2">
         {#each posts as post}
-            {#if selectedValue === "Select a Topic" || post.topic === selectedValue.toLowerCase()}
+            {#if selectedValue === "Select a Topic" || post.topic.name === selectedValue.toLowerCase()}
                 <Card.Root class="my-2 w-[350px]">
                     <Card.Header>
                         <Card.Title>{post.title}</Card.Title>
-                        <Card.Description>{post.topic}</Card.Description>
+                        <!-- <Card.Description>{post.topic.name}</Card.Description> -->
                     </Card.Header>
                     <Card.Content>
                         <form>
@@ -198,10 +229,15 @@
                             </div>
                         </form>
                     </Card.Content>
-                    <!-- <Card.Footer class="flex justify-between"> -->
-                    <!--     <Button variant="outline">Cancel</Button> -->
-                    <!--     <Button>Deploy</Button> -->
-                    <!-- </Card.Footer> -->
+                    <Card.Footer class="flex justify-between">
+                        <Card.Description>{post.owner.name}</Card.Description>
+                        {#if post.owner.id == user_id}
+                            <Button
+                                variant="destructive"
+                                on:click={deletePost(post.id.id)}>Delete</Button
+                            >
+                        {/if}
+                    </Card.Footer>
                 </Card.Root>
             {/if}
         {/each}
